bf7ab69c5fe236e8292fec9bbc6319590d425652
Arnau Mora <arnyminer.z@gmail.com>
2022-10-25 14:08:39 +0200

    :broom: Cleaned up code
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- markdown-text/src/main/java/com/arnyminerz/markdowntext/MarkdownText.kt
+++ markdown-text/src/main/java/com/arnyminerz/markdowntext/MarkdownText.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
