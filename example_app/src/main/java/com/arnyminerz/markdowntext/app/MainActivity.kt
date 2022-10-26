caa0ded2fc36ddb41c4d2014960d6b769bf29480
Arnau Mora <Arnyminer.Z@gmail.com>
2022-10-26 20:00:30 +0200

    :sparkles: Added example app (#9)
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ example_app/src/main/java/com/arnyminerz/markdowntext/app/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.arnyminerz.markdowntext.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.arnyminerz.markdowntext.MarkdownTextPreview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            // A surface container using the 'background' color from the theme
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                MarkdownTextPreview()
+            }
+        }
+    }
+}
