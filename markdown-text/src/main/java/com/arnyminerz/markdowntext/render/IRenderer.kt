22f430df0b389b548d9c6eac07116cd6c4705333
Arnau Mora <arnyminerz@proton.me>
2024-05-22 17:30:15 +0200

    Improved KDoc
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- markdown-text/src/main/java/com/arnyminerz/markdowntext/render/IRenderer.kt
+++ markdown-text/src/main/java/com/arnyminerz/markdowntext/render/IRenderer.kt
@@ -4,10 +4,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.AnnotatedString.Builder
 import com.arnyminerz.markdowntext.component.model.Feature
+import com.arnyminerz.markdowntext.MarkdownContainer
+import com.arnyminerz.markdowntext.MarkdownText
 
 interface IRenderer<FeatureType : Feature> {
     /**
      * Renders the content of the feature as a standalone component.
+     *
+     * **Used by [MarkdownContainer]**
      * @param feature The feature to render.
      * @param modifier The modifier to apply to the component.
      */
@@ -18,6 +22,8 @@ interface IRenderer<FeatureType : Feature> {
      * Provides a way to append the feature to the [annotatedStringBuilder].
      *
      * Used for instances where the feature can be rendered directly inside of a Text component.
+     *
+     * **Used by [MarkdownText]**
      * @param annotatedStringBuilder The [Builder] to append the feature to.
      * @param feature The feature to append.
      * @return The [Builder] with the appended feature.
